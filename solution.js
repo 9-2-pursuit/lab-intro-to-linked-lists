cacde62b179d58cf431b94cd3b94927fb8345db2
Karyn Huston <huston.karyn@gmail.com>
2023-04-09 03:16:21 -0400

    can create a new node

--- solution.js
+++ solution.js
@@ -1,5 +1,24 @@
 const { nums, words } = require("./data/data.js");
 
+class Node{
+  constructor(data = null) {
+this.data = data;
+this.next = null;
+  }
+}
+
+class LinkedList{
+  constructor(head) {
+this.head = head;
+  }
+
+  insert(head = this.head, data) {
+    return head
+  }
+}
+
+ 
+
 module.exports = {
   Node,
   LinkedList,
