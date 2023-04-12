1b0cb7b565e3e7f7c19958ff48048a4c5199c350
Karyn Huston <huston.karyn@gmail.com>
2023-04-12 03:29:35 -0400

    get length of list

--- solution.js
+++ solution.js
@@ -1,23 +1,40 @@
 const { nums, words } = require("./data/data.js");
 
-class Node{
-  constructor(data = null) {
-this.data = data;
-this.next = null;
+
+class Node {
+  constructor(data = null) {//data is default set to null 
+    this.data = data; //when a instance of a node is created. The value of the data is assigned to the property name data
+    this.next = null; //the pointer is default at null unless instanitiated
   }
 }
 
-class LinkedList{
-  constructor(head) {
-this.head = head;
+class LinkedList {
+  constructor(head = null) { 
+    this.head = head;
+
+
   }
 
-  insert(head = this.head, data) {
-    return head
+  insert(data) { 
+
+    let newNode = new Node(data)
+    newNode.next = this.head;
+    this.head = newNode;
   }
+
+ size() {
+    let count = 0;
+    let node = this.head;
+    while(node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+ }
+
+
+
 }
-
- 
 
 module.exports = {
   Node,
